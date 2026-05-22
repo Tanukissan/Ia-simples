6f2040852769191167766196eb36c6264c998da8
Daniel Kendy <danieltakedabueno@gmail.com>
2026-05-22 16:12:32 -0300

    Refinamento na limpeza do nome e correções de captação

--- chat.py
+++ chat.py
@@ -1,28 +1,33 @@
+from string import punctuation
+
 nome = ""
 resposta = input("Saudações caro viajante, poderia me dizer seu nome? ").lower()
 
+
 if 'nome é' in resposta:
-    nome = resposta [resposta.find('nome é') + 6:].lstrip()
-    nome = nome.replace("."and ","and ";"and "-"and "!"and "?"and "("and ")", " ")
-    nome = nome [0 :nome.find(" ") + 1:].strip().capitalize()
-    print("Seja bem-vindo, " + nome + "!")
+    nome = resposta [resposta.find('nome é') + 6:].lstrip()            #Coloca o nome do usuário no começo do texto
+    nome = "".join([char for char in nome if char not in punctuation]) #Limpa os caracteres especiais da frase
+    nome = nome [0 :nome.find(" ") + 1:].strip().capitalize()          #Deixa somente o primeiro nome do usuário na variável
+    print("Seja bem-vindo, {}!".format(nome))
+
+#Os blocos a baixo tem a mesma função, mas para outras estruturas de frases
 
 elif 'chamo' in resposta:
     nome = resposta [resposta.find('chamo') + 5:].lstrip()
-    nome = nome.replace("."and ","and ";"and "-"and "!"and "?"and "("and ")", " ")
+    nome = "".join([char for char in nome if char not in punctuation])
     nome = nome [0 :nome.find(" ") + 1:].strip().capitalize()
-    print("Seja bem-vindo, " + nome + "!")
+    print("Seja bem-vindo, {}!".format(nome))
 
 else:
-    reposta = input("Você se chama mesmo " + resposta.capitalize() + "?").lower()
+    reposta = input("Você se chama mesmo {}?".format(resposta.capitalize())).lower()
 
     if 'sim' in reposta:
         nome = resposta.capitalize()
-        print ("Seja bem-vindo, " + nome + "!")
+        print ("Seja bem-vindo, {}!".format(nome))
 
     else:
         nome = input("Poderia me dizer somente o seu nome por favor?").capitalize()
-        print ("Seja bem-vindo, " + nome + "!")
+        print ("Seja bem-vindo, {}!".format(nome))
 
 
 #while 'encerrar conversa' not in resposta:
