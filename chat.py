c7b43774c228d203da6989a49b398210bb211d16
Daniel Kendy <danieltakedabueno@gmail.com>
2026-05-22 15:38:01 -0300

    Atualizações na captação do nome

--- chat.py
+++ chat.py
@@ -1,17 +1,16 @@
 nome = ""
-idade = 0
-altura = 0.0
-peso = 0.0
-genero = ""
-
-resposta = input("Saudações caro viajante, poderia me dizer seu nome?").lower()
+resposta = input("Saudações caro viajante, poderia me dizer seu nome? ").lower()
 
 if 'nome é' in resposta:
-    nome = resposta [resposta.find('nome é') + 6:].strip().capitalize()
+    nome = resposta [resposta.find('nome é') + 6:].lstrip()
+    nome = nome.replace("."and ","and ";"and "-"and "!"and "?"and "("and ")", " ")
+    nome = nome [0 :nome.find(" ") + 1:].strip().capitalize()
     print("Seja bem-vindo, " + nome + "!")
 
 elif 'chamo' in resposta:
-    nome = resposta [resposta.find('chamo') + 5:].strip().capitalize()
+    nome = resposta [resposta.find('chamo') + 5:].lstrip()
+    nome = nome.replace("."and ","and ";"and "-"and "!"and "?"and "("and ")", " ")
+    nome = nome [0 :nome.find(" ") + 1:].strip().capitalize()
     print("Seja bem-vindo, " + nome + "!")
 
 else:
@@ -24,3 +23,9 @@ else:
     else:
         nome = input("Poderia me dizer somente o seu nome por favor?").capitalize()
         print ("Seja bem-vindo, " + nome + "!")
+
+
+#while 'encerrar conversa' not in resposta:
+#    resposta = input("O que mais você gostaria de me contar sobre você?").lower()
+    
+    
