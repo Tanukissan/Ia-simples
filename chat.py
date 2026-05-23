1691d4eb26d54b549b33005496e1670a483d19f1
Daniel Kendy <danieltakedabueno@gmail.com>
2026-05-23 17:05:07 -0300

    Foi criada uma nova função para otimizar o código e dar mais flexibilidade

--- chat.py
+++ chat.py
@@ -1,36 +1,35 @@
 from string import punctuation
 
 nome = ""
-resposta = input("Saudações caro viajante, poderia me dizer seu nome? ").lower()
+resposta = ""
 
 def limpa_char(itemA):  #Limpa os caracteres especiais da frase
     itemA = "".join([char for char in itemA if char not in punctuation]) 
     itemA = itemA [0 :itemA.find(" ") + 1:].strip().capitalize()
+    return itemA
 
-
-if 'nome é' in resposta:
-    nome = resposta [resposta.find('nome é') + 6:].lstrip()            #Coloca o nome do usuário no começo do texto
-    nome = limpa_char(nome)
-    print("Seja bem-vindo, {}!".format(nome))
-
-#Os blocos a baixo tem a mesma função, mas para outras estruturas de frases
-
-elif 'chamo' in resposta:
-    nome = resposta [resposta.find('chamo') + 5:].lstrip()
-    nome = limpa_char(nome)
-    print("Seja bem-vindo, {}!".format(nome))
-
-else:
-    reposta = input("Você se chama mesmo {}?".format(resposta.capitalize())).lower()
-
-    if 'sim' in reposta:
-        nome = resposta.capitalize()
-        print ("Seja bem-vindo, {}!".format(nome))
+def chama_nome(frase):
+    if 'nome é' in frase:
+        nome = frase [frase.find('nome é') + 6:].lstrip()
+        nome = limpa_char(nome)
+        
+    elif 'chamo' in frase:
+        nome = frase [frase.find('chamo') + 5:].lstrip()
+        nome = limpa_char(nome)
 
     else:
-        nome = input("Poderia me dizer somente o seu nome por favor?").capitalize()
-        print ("Seja bem-vindo, {}!".format(nome))
+        frase = input("Você se chama mesmo {}?".format(frase.capitalize())).lower()
 
+        if 'sim' in frase:
+            nome = frase.capitalize()
+            
+        else:
+            nome = input("Poderia me dizer somente o seu nome por favor?").capitalize()
+    return nome
+
+resposta = input("Poderia me dizer seu nome? ").lower()
+nome = chama_nome(resposta)
+print("Prazer em te conhecer, {}!".format(nome))
 
 #while 'encerrar conversa' not in resposta:
 #    resposta = input("O que mais você gostaria de me contar sobre você?").lower()
